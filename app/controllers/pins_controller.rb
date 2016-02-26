814a71e64cef0b6bbd757f57d2e66dc1b24279bd
Antonio de Leon <tonichi@gmail.com>
2016-02-25 23:45:03 -0800

    fixed bug when app is checking if user owns pin before checking if logged in

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,8 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action and before_filter are the same thing 
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
@@ -21,9 +20,9 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.' 
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render action: 'new' 
+      render :new
     end
   end
 
@@ -31,30 +30,28 @@ class PinsController < ApplicationController
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render action: 'edit'
+      render :edit
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url   
+    redirect_to pins_url
   end
 
   private
-
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
 
     def correct_user
-      @pin = current_user.pins.find_by(params[:id])
-      redirect_to pins_path, notice: "Note authorized to edit this pin" if @pin.nil?
-    end 
-    # find_by doesn't raise an error if using find_by vs find
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:descriptioin)
+      params.require(:pin).permit(:description, :image)
     end
 end
