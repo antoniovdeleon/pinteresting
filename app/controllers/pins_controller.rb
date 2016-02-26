3122c756818114c4feeafe223cf5561ebc23cfd0
Antonio de Leon <tonichi@gmail.com>
2016-02-26 00:43:27 -0800

    fixed bug with pin controller & deleted devise_helper override method

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
     @pins = Pin.all
@@ -52,6 +53,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:descriptioin, :image)
     end
 end
