07c58fff16a94dea34b64c7770628efbbfc27833
Antonio de Leon <tonichi@gmail.com>
2016-03-18 18:02:30 -0700

    explicitly defining bucket in associatoin has_attached_file for pin.rb for amazons3 bucket

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,10 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, 
+      :styles => { :medium => "300x300>", 
+      :thumb => "100x100>" }, 
+      :bucket => ENV['pinterestization.herokuapp']
+      default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
 
