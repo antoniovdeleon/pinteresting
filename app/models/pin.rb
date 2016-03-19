2979691a94c6298b59beba35d306d785e2893bb2
Antonio de Leon <tonichi@gmail.com>
2016-03-18 18:26:33 -0700

    took out bucket name, accessId, and secret access key from production.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,8 +3,8 @@ class Pin < ActiveRecord::Base
     has_attached_file :image, 
       :styles => { :medium => "300x300>", 
       :thumb => "100x100>" }, 
-      :storage => :s3,
-      :bucket => ENV['pinterestization.herokuapp'],
+      # :storage => :s3,
+      # :bucket => ENV['pinterestization.herokuapp'],
       default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['pinterization.herokuapp.com'],
-      :access_key_id => ENV['AKIAJH5TCN4MXX54BHRQ'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
